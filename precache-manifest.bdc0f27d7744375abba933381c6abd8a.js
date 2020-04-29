fb4fa6dc786bc6a1890dcb87d165a0c5113c7873
brandon <contactbrandonye@gmail.com>
2020-04-29 00:19:24 -0400

    Updates

---
+++ precache-manifest.bdc0f27d7744375abba933381c6abd8a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29cf2d8a22405f7e130681a2c918087a",
+    "url": "/sodaintheoven.online/index.html"
+  },
+  {
+    "revision": "681de913755fd948fed0",
+    "url": "/sodaintheoven.online/static/css/main.764fdc66.chunk.css"
+  },
+  {
+    "revision": "e84d9f0575c0667bf3ff",
+    "url": "/sodaintheoven.online/static/js/2.f96721e7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/sodaintheoven.online/static/js/2.f96721e7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "681de913755fd948fed0",
+    "url": "/sodaintheoven.online/static/js/main.bf07c6f0.chunk.js"
+  },
+  {
+    "revision": "b1a801bf267fe6480ff5",
+    "url": "/sodaintheoven.online/static/js/runtime-main.bfdef384.js"
+  }
+]);
